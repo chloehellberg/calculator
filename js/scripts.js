87c592d658f2a83a3337d00b4e0e1cd0e490d979
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-07-02 20:15:58 -0700

    removing alert and replacing it with an output result

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,26 @@
 function add(number1, number2) {
   return number1 + number2;
-}
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
-
-
-
+};
 
 function subtract(number1, number2) {
   return number1 - number2;
-}
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
-alert(result);
-
-
-
-
+};
 
 function multiply(number1, number2) {
   return number1 * number2;
-}
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = multiply(number1, number2);
-alert(result);
-
-
-
-
+};
 
 function divide(number1, number2) {
   return number1 / number2;
-}
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = divide(number1, number2);
-alert(result);
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
